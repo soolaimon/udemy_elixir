893504baf12eb5031c19a9abf346ae67795d9505
soolaimon <davidicus21@gmail.com>
2016-12-05 20:19:52 -0700

    doesn't matter

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,12 +1,26 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
-  def contains?(card) do
+  def contains?([], _), do: false
+  def contains?([ head | _ ], card) when card == head, do: true
+  def contains?([ _ | deck ], card), do: contains?(deck, card)
+
+  # def contains?(deck, card) do
+  #   Enum.member?(deck, card)
+  # end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
